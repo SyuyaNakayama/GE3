501e3f2dac0d7fae5a7082f11226eb6ec45c951c
SyuyaNakayama <k021g1352@g.neec.ac.jp>
2022-11-29 13:02:49 +0900

    リファクタリング1

--- SourceFiles/Sprite.cpp
+++ SourceFiles/Sprite.cpp
@@ -5,17 +5,44 @@
 using namespace Microsoft::WRL;
 using namespace DirectX;
 
+template<class T> void BufferMapping(ID3D12Resource** constBuff, T** constMapData)
+{
+	D3D12_HEAP_PROPERTIES cbHeapProp{};
+	cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
+
+	D3D12_RESOURCE_DESC cbResourceDesc{};
+	cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	cbResourceDesc.Width = (sizeof(T) + 0xff) & ~0xff;
+	cbResourceDesc.Height = 1;
+	cbResourceDesc.DepthOrArraySize = 1;
+	cbResourceDesc.MipLevels = 1;
+	cbResourceDesc.SampleDesc.Count = 1;
+	cbResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	HRESULT result = DirectXCommon::GetInstance()->GetDevice()->CreateCommittedResource(
+		&cbHeapProp, // ヒープ設定
+		D3D12_HEAP_FLAG_NONE,
+		&cbResourceDesc, // リソース設定
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(constBuff));
+	assert(SUCCEEDED(result));
+
+	result = (*constBuff)->Map(0, nullptr, (void**)constMapData);
+	assert(SUCCEEDED(result));
+}
+
 void Sprite::Initialize()
 {
 	HRESULT result;
 	ID3D12Device* device = DirectXCommon::GetInstance()->GetDevice();
 #pragma region 頂点バッファ
 	vertices.push_back({ { 0.0f,   100.0f }, { 0.0f, 1.0f } }); // 左下
-	vertices.push_back({ { 0.0f,   0.0f  }, { 0.0f, 0.0f } }); // 左上
+	vertices.push_back({ { 0.0f,     0.0f }, { 0.0f, 0.0f } }); // 左上
 	vertices.push_back({ { 100.0f, 100.0f }, { 1.0f, 1.0f } }); // 右下
 	vertices.push_back(vertices[2]); // 右下
 	vertices.push_back(vertices[1]); // 左上
-	vertices.push_back({ { 100.0f, 0.0f }, { 1.0f, 0.0f } }); // 右上
+	vertices.push_back({ { 100.0f,   0.0f }, { 1.0f, 0.0f } }); // 右上
 	// 頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
 	UINT sizeVB = static_cast<UINT>(sizeof(vertices[0]) * vertices.size());
 
@@ -62,78 +89,29 @@ void Sprite::Initialize()
 	vbView.StrideInBytes = sizeof(vertices[0]);
 #pragma endregion
 #pragma region 定数バッファ
-	D3D12_HEAP_PROPERTIES cbHeapProp{};
-	cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
-
-	D3D12_RESOURCE_DESC cbResourceDesc{};
-	cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-	cbResourceDesc.Width = (sizeof(ConstBufferDataMaterial) + 0xff) & ~0xff;
-	cbResourceDesc.Height = 1;
-	cbResourceDesc.DepthOrArraySize = 1;
-	cbResourceDesc.MipLevels = 1;
-	cbResourceDesc.SampleDesc.Count = 1;
-	cbResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-
-	result = device->CreateCommittedResource(
-		&cbHeapProp, // ヒープ設定
-		D3D12_HEAP_FLAG_NONE,
-		&cbResourceDesc, // リソース設定
-		D3D12_RESOURCE_STATE_GENERIC_READ,
-		nullptr,
-		IID_PPV_ARGS(&constBuffMaterial));
-	assert(SUCCEEDED(result));
-
 	ConstBufferDataMaterial* constMapMaterial = nullptr;
-	result = constBuffMaterial->Map(0, nullptr, (void**)&constMapMaterial);
-	assert(SUCCEEDED(result));
+	BufferMapping<ConstBufferDataMaterial>(constBuffMaterial.GetAddressOf(), &constMapMaterial);
+	constMapMaterial->color = XMFLOAT4(1, 1, 1, 1);
 
-	constMapMaterial->color = XMFLOAT4(1,1,1,1);
-
-	{
-		D3D12_HEAP_PROPERTIES cbHeapProp{};
-		cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
-
-		D3D12_RESOURCE_DESC cbResourceDesc{};
-		cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-		cbResourceDesc.Width = (sizeof(ConstBufferDataTransform) + 0xff) & ~0xff;
-		cbResourceDesc.Height = 1;
-		cbResourceDesc.DepthOrArraySize = 1;
-		cbResourceDesc.MipLevels = 1;
-		cbResourceDesc.SampleDesc.Count = 1;
-		cbResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-
-		result = device->CreateCommittedResource(
-			&cbHeapProp, // ヒープ設定
-			D3D12_HEAP_FLAG_NONE,
-			&cbResourceDesc, // リソース設定
-			D3D12_RESOURCE_STATE_GENERIC_READ,
-			nullptr,
-			IID_PPV_ARGS(&constBuffTransform));
-		assert(SUCCEEDED(result));
-
-		result = constBuffTransform->Map(0, nullptr, (void**)&constMapTransform);
-		assert(SUCCEEDED(result));
-	}
+	BufferMapping<ConstBufferDataTransform>(constBuffTransform.GetAddressOf(), &constMapTransform);
 
 	// 平行投影行列の生成
 	matProj.r[0].m128_f32[0] = 2.0f / WindowsAPI::WIN_WIDTH;
 	matProj.r[1].m128_f32[1] = -2.0f / WindowsAPI::WIN_HEIGHT;
 	matProj.r[3].m128_f32[0] = -1.0f;
 	matProj.r[3].m128_f32[1] = 1.0f;
-
-	XMMATRIX matRot, matTrans;
-	matRot = matTrans = XMMatrixIdentity();
-
-	matRot *= XMMatrixRotationZ(rotation);
-	matTrans = XMMatrixTranslation(position.x, position.y, 0);
-
-	matWorld *= matRot * matTrans;
-	constMapTransform->mat = matWorld * matProj;
 #pragma endregion
 }
 
 void Sprite::Update()
 {
+	XMMATRIX matRot, matTrans;
+	matRot = XMMatrixRotationZ(rotation);
+	matTrans = XMMatrixTranslation(position.x, position.y, 0);
+
+	matWorld = XMMatrixIdentity();
+	matWorld *= matRot * matTrans;
+	constMapTransform->mat = matWorld * matProj;
 }
 
 void Sprite::Draw()
